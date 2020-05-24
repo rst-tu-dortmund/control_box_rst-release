14be9cf6930f784a08f4c579df914a6244016ac7
Christoph Rösmann <christoph.roesmann@tu-dortmund.de>
2020-05-24 11:00:03 +0200

    Rebase from 'debian/melodic/control_box_rst'

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -2,6 +2,32 @@
 Changelog for package control_box_rst
 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
+0.0.6 (2020-05-24)
+------------------
+* Hybrid cost functions added: Minimum time and control/state quadratic form
+* Discretization grids: The time difference is now initialized to dt_ref for proper reference caching
+* Contributors: Christoph Rösmann
+
+0.0.5 (2020-05-13)
+------------------
+* Changed minimum CMake version to 3.1
+* Add default parameter to SolverIpopt::initialize() in case Ipopt is not found..
+* Removed obsolete gmock include
+* Fix setLastControlRef and graph consistency.
+* Internal libgtest target renamed. Fixed compilation issue for yaml-cpp test
+* Contributors: Christoph Rösmann, Maximilian Krämer
+
+0.0.4 (2020-02-21)
+------------------
+* Removed exec_depend on catkin to allow the use of the same release for ros1 and ros2
+* Contributors: Christoph Rösmann
+
+0.0.3 (2020-02-21)
+------------------
+* Changed build_type to cmake in package.xml according to REP-136
+* Set some optional findPackage cmake scripts to quiet
+* Contributors: Christoph Rösmann
+
 0.0.2 (2020-02-19)
 ------------------
 * Changed ros dependency from eigen3 to eigen
--- src/optimal_control/src/structured_ocp/discretization_grids/shooting_grid_base.cpp
+++ src/optimal_control/src/structured_ocp/discretization_grids/shooting_grid_base.cpp
@@ -56,6 +56,7 @@ GridUpdateResult ShootingGridBase::update(const Eigen::VectorXd& x0, ReferenceTr
     // check if we need to cache the reference trajectory values // TODO(roesmann): we could restrict this to new_run==true only as long as we do not
     // have a grid resize...
     int n = std::max(std::max(getNRef(), getN()), _n_adapt);
+    if (_dt.value() <= 0) _dt.value() = _dt_ref; // initialize _dt to _dt_ref in case it has not been optimized before
     if (!xref.isCached(getDt(), n, t)) xref.precompute(getDt(), n, t);
     if (!uref.isCached(getDt(), n, t)) uref.precompute(getDt(), n, t);
     if (sref && !sref->isCached(getDt(), n, t)) sref->precompute(getDt(), n, t);
@@ -69,7 +70,7 @@ GridUpdateResult ShootingGridBase::update(const Eigen::VectorXd& x0, ReferenceTr
         setPreviousControl(Eigen::VectorXd::Zero(dynamics->getInputDimension()), prev_u_dt);
 
     // set last control to uref
-    setLastControlRef(uref.getReferenceCached(n));
+    setLastControlRef(uref.getReferenceCached(n - 1));
 
     if (_num_u_per_interv_ref <= 1)
         _full_discretization = true;
@@ -575,6 +576,8 @@ void ShootingGridBase::getVertices(std::vector<VertexInterface*>& vertices)
     if (isXfShootingNode()) vertices.push_back(&_xf);  // TODO(roesmann) check if xf_shooting_node is implemented correctly everywhere
     vertices.push_back(&_dt);                          // make sure to make it fixed if desired in any subclass
     vertices.push_back(&_u_prev);                      // always fixed...
+    vertices.push_back(&_u_ref);
+    vertices.push_back(&_u_prev_dt);
 }
 
 void ShootingGridBase::computeActiveVertices()
